4190c087a08cefbef2a0a4aa861c893f2aaeeb83
citcwalker <citcwalker@cau.ac.kr>
2025-12-13 04:10:10 +0900

    Create GuildMessageEvent

--- src/main/java/kr/ac/cau/citcwalker/minecraft/egen5/discord/Commands.java
+++ src/main/java/kr/ac/cau/citcwalker/minecraft/egen5/discord/Commands.java
@@ -5,6 +5,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Commands extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
-
+        if(event.isFromGuild()) {
+            //Guild Message
+        }
     }
 }

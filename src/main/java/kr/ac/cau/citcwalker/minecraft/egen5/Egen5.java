fbcc98cb206ea01cd227a71efafa85f6846dd25a
citcwalker <citcwalker@cau.ac.kr>
2025-12-13 18:05:05 +0900

    Added ReturnSkul

--- src/main/java/kr/ac/cau/citcwalker/minecraft/egen5/Egen5.java
+++ src/main/java/kr/ac/cau/citcwalker/minecraft/egen5/Egen5.java
@@ -1,19 +1,31 @@
 package kr.ac.cau.citcwalker.minecraft.egen5;
 
 import kr.ac.cau.citcwalker.minecraft.egen5.discord.Commands;
+import kr.ac.cau.citcwalker.minecraft.egen5.item.ReturnSkul;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Egen5 extends JavaPlugin {
+import java.util.EventListener;
+
+public final class Egen5 extends JavaPlugin implements Listener {
 
     private JDA jda;
     private EmbedBuilder embed = new EmbedBuilder();
+    private ReturnSkul returnSkul;
 
     @Override
     public void onEnable() {
@@ -23,6 +35,9 @@ public final class Egen5 extends JavaPlugin {
         jda.addEventListener(new Commands());
         jda.getGuildById(Datas.guildID).getTextChannelById(Datas.channelID).sendMessageEmbeds(embed.setTitle("서버 켜짐").setColor(0x00ff00).setDescription("The Server is online.\nServer ip : 25.32.194.9:25565").build()).queue();
         embed.clear();
+
+        //Enable Event Handler
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -34,6 +49,29 @@ public final class Egen5 extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(label.equalsIgnoreCase("returnskul") && sender instanceof Player) {
+            ((Player) sender).getInventory().addItem(returnSkul);
+        }
         return false;
+    }
+
+    @EventHandler
+    public void onPlayerClicks(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Action action = event.getAction();
+        ItemStack item = event.getItem();
+
+        if ( action.equals( Action.RIGHT_CLICK_AIR ) || action.equals( Action.RIGHT_CLICK_BLOCK ) ) {
+            if ( item != null && item == returnSkul ) {
+                player.performCommand("/home home");
+                ItemStack hand =  player.getInventory().getItemInMainHand();
+                if(hand.getAmount() == 1) {
+                    player.getInventory().setItemInMainHand(null);
+                }else {
+                    hand.setAmount(hand.getAmount() - 1);
+                }
+            }
+        }
+
     }
 }
--- src/main/java/kr/ac/cau/citcwalker/minecraft/egen5/item/ReturnSkul.java
+++ src/main/java/kr/ac/cau/citcwalker/minecraft/egen5/item/ReturnSkul.java
@@ -1,4 +1,27 @@
 package kr.ac.cau.citcwalker.minecraft.egen5.item;
 
-public class ReturnSkul {
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+
+public class ReturnSkul extends ItemStack {
+    private ItemMeta meta;
+    private List<String> lore;
+    public ReturnSkul() {
+        super(Material.PAPER);
+        meta.setItemName("§b귀환석");
+        lore.add("§7집으로 돌아갈 수 있는 귀환석이다.");
+        lore.add("§7누군진 모르겠는데 윤현이 만들어달라고 했다.");
+        lore.add("§7사유는 잘 모르겠는데 sethome을 넣어도 /home을 치기 귀찮으시댄다.");
+        meta.setLore(lore);
+        meta.addEnchant(Enchantment.PROTECTION, 1, false);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        setItemMeta(meta);
+    }
 }
